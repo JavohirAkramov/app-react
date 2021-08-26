c3fa01786827a1831770b34fc2b3212298372a62
JavohirAkramov <javohirakramov@mail.ru>
2021-08-26 19:19:42 +0500

    Users pagination

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,11 +1,14 @@
 import react from 'react';
 import s from './Users.module.css';
-import {followAC, unfollowAC, setUsersAC} from './../../redux/users-reducer';
+import {followAC, unfollowAC, setUsersAC, setCurrentPageAC, setTotalUsersCountAC} from './../../redux/users-reducer';
 import {connect} from 'react-redux';
 import Users from './Users';
 
 let mapStateToProps = (state) => ({
-  users: state.usersPage.users
+  users: state.usersPage.users,
+  pageSize: state.usersPage.pageSize,
+  totalUsersCount: state.usersPage.totalUsersCount,
+  currentPage: state.usersPage.currentPage
 });
 
 let mapDispatchToProps = (dispatch) => ({
@@ -17,6 +20,12 @@ let mapDispatchToProps = (dispatch) => ({
   },
   setUsers: (users) => {
     dispatch(setUsersAC(users))
+  },
+  setCurrentPage: (pageNumber) => {
+    dispatch(setCurrentPageAC(pageNumber))
+  },
+  setTotalUsersCount: (totalUsersCount) => {
+    dispatch(setTotalUsersCountAC(totalUsersCount))
   }
 })
 
