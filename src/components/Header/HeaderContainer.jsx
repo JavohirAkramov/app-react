4c09c9b479133e07e531191b85a41104ee2ea1ed
JavohirAkramov <javohirakramov@mail.ru>
2021-09-06 17:53:44 +0500

    error is handled

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -8,7 +8,7 @@ import {connect} from 'react-redux';
 class HeaderContainer extends React.Component {
   componentDidMount() {
     axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {
-      withCredintials: true
+      withCredentials: true
     })
       .then(response => {
         console.log(response)
