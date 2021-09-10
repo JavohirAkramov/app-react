b024ea5ad7b5b2fb7ab880fb6b255078e9334a00
JavohirAkramov <javohirakramov@mail.ru>
2021-09-10 16:34:10 +0500

    ---

--- src/API/API.js
+++ src/API/API.js
@@ -14,9 +14,13 @@ export const getUsers = (currentPage = 1, pageSize = 10) => {
 }
 export const unfollowUser = (userId) => {
   return instance.delete(`follow/${userId}`)
-    .then(response => response.data)
+    .then(response => {
+    return response.data
+    })
 }
 export const followUser = (userId) => {
   return instance.post(`follow/${userId}`, {})
-    .then(response => response.data)
+    .then(response => {
+      return response.data
+    })
 }
