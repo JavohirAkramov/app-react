1f9b77e56cec36d20dc4199627a07ac8f8df9a72
JavohirAkramov <javohirakramov@mail.ru>
2021-09-15 23:27:58 +0500

    .

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -7,16 +7,7 @@ import {connect} from 'react-redux';
 
 class HeaderContainer extends React.Component {
   componentDidMount() {
-    this.props.setAuthUserData()
-    // axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {
-    //   withCredentials: true
-    // })
-    //   .then(response => {
-    //     if(response.data.resultCode === 0) {
-    //       let {id, email, login} = response.data.data
-    //       this.props.setAuthUserData(id, email, login);
-    //     }
-    //   })
+    this.props.setAuthUserData();
   }
   render() {
     return <Header {...this.props}/>
