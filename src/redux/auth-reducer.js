dba4cb014459f525e5372a147a9b03d34c4cb6c8
JavohirAkramov <javohirakramov@mail.ru>
2021-11-16 23:32:14 +0500

    s

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -12,7 +12,7 @@ let initialState = {
 export const authReducer = (state = initialState, action) => {
   switch(action.type) {
     case SET_AUTH_USER_DATA: {
-      return {...state, ...action.data, isAuth: false}
+      return {...state, ...action.data, isAuth: true}
     }
     default:
       return state;
