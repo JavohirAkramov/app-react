b5d0c32c380f4e4d854e5805a6ea808148d3f5ca
JavohirAkramov <javohirakramov@mail.ru>
2021-11-22 10:56:15 +0500

    state is remembercopied

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,19 +1,21 @@
-import react from 'react';
+import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from '../../commons/Preloader/Preloader';
+import ProfileStatus from './ProfileStatus.jsx'
 
 const ProfileInfo = (props) => {
   if(!props.profile) return <Preloader />
   return (
     <div className={s.content}>
-        <img className={s.img} src=" "/>
+        {/*<img className={s.img} src=" "/>*/}
         <div className={s.descriptionBlock}>
           <img src={props.profile.photos.large}/>
-          {props.profile.userId}<br/>
+          {/*{props.profile.userId}<br/>
           {props.profile.fullName}<br/>
           {props.profile.contacts.facebook}<br/>
           {props.profile.lookingForAJobDescription}<br/>
-          avatar + description
+          avatar + description*/}
+          <ProfileStatus status={'this is status'}/>
         </div>
       </div>
   );
