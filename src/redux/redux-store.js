2ed8cf97bc5baea87b6a79487bb5e0fe5a1badc3
JavohirAkramov <javohirakramov@mail.ru>
2022-02-09 00:25:09 +0500

    commit

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,12 +3,9 @@ import {profileReducer} from './profile-reducer';
 import {messagesReducer} from './messages-reducer';
 import {sidebarReducer} from './sidebar-reducer';
 import {usersReducer} from './users-reducer';
-<<<<<<< HEAD
 import {authReducer} from './auth-reducer';
-=======
 import {authReducer}from './auth-reducer';
 import thunkMiddleware from 'redux-thunk';
->>>>>>> app-br-remembered
 
 
 let reducers = combineReducers({
